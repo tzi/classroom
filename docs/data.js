b083dfa2dbe2b2cf2fbdd2b1bd64b039471e70ad
Thomas ZILLIOX <thomas@zilliox.me>
2023-11-29 16:41:28 +0100

    Add more sounds

--- docs/data.js
+++ docs/data.js
@@ -1,6 +1,10 @@
 const data = {
     soundList: [
         {
+            src: 'alors.mp3',
+            answer: 'alors'
+        },
+        {
             src: 'apres.mp3',
             answer: 'après'
         },
@@ -15,6 +19,22 @@ const data = {
         {
             src: 'autour.mp3',
             answer: 'autour'
+        },
+        {
+            src: 'avoir.mp3',
+            answer: 'avoir'
+        },
+        {
+            src: 'belle.mp3',
+            answer: 'belle'
+        },
+        {
+            src: 'blanc.mp3',
+            answer: 'blanc'
+        },
+        {
+            src: 'bleu.mp3',
+            answer: 'bleu'
         },
         {
             src: 'bonjour.mp3',
@@ -37,6 +57,14 @@ const data = {
             answer: 'dessous'
         },
         {
+            src: 'du-chocolat.mp3',
+            answer: 'du-chocolat'
+        },
+        {
+            src: 'du-lait.mp3',
+            answer: 'du-lait'
+        },
+        {
             src: 'du-sucre.mp3',
             answer: 'du sucre'
         },
@@ -51,6 +79,18 @@ const data = {
         {
             src: 'je-suis.mp3',
             answer: 'je suis'
+        },
+        {
+            src: 'joyeux.mp3',
+            answer: 'joyeux'
+        },
+        {
+            src: 'l-ecola.mp3',
+            answer: 'l’école'
+        },
+        {
+            src: 'la-reine.mp3',
+            answer: 'la reine'
         },
         {
             src: 'la-couleur.mp3',
@@ -77,8 +117,24 @@ const data = {
             answer: 'la rue'
         },
         {
+            src: 'le-bois.mp3',
+            answer: 'le bois'
+        },
+        {
             src: 'la-repas.mp3',
             answer: 'la repas'
+        },
+        {
+            src: 'la-roi.mp3',
+            answer: 'le roi'
+        },
+        {
+            src: 'loin.mp3',
+            answer: 'loin'
+        },
+        {
+            src: 'ma-lecon.mp3',
+            answer: 'ma leçon'
         },
         {
             src: 'ma-mere.mp3',
@@ -134,6 +190,10 @@ const data = {
             answer: 'toujours'
         },
         {
+            src: 'un-animal.mp3',
+            answer: 'un animal'
+        },
+        {
             src: 'un-arbre.mp3',
             answer: 'un arbre'
         },
@@ -146,12 +206,28 @@ const data = {
             answer: 'un fruit'
         },
         {
+            src: 'un-livre.mp3',
+            answer: 'un livre'
+        },
+        {
             src: 'un-nuage.mp3',
             answer: 'un nuage'
         },
         {
             src: 'un-pull.mp3',
             answer: 'un pull'
+        },
+        {
+            src: 'un-voyage.mp3',
+            answer: 'un voyage'
+        },
+        {
+            src: 'une-balle.mp3',
+            answer: 'une balle'
+        },
+        {
+            src: 'une-couleur.mp3',
+            answer: 'une couleur'
         },
         {
             src: 'une-journee.mp3',
@@ -162,12 +238,28 @@ const data = {
             answer: 'une plume'
         },
         {
+            src: 'une-rue.mp3',
+            answer: 'une souris'
+        },
+        {
             src: 'une-souris.mp3',
             answer: 'une souris'
         },
         {
+            src: 'une-ville.mp3',
+            answer: 'une ville'
+        },
+        {
             src: 'une-voiture.mp3',
             answer: 'une voiture'
+        },
+        {
+            src: 'voici.mp3',
+            answer: 'voici'
+        },
+        {
+            src: 'voilà.mp3',
+            answer: 'voilà'
         },
     ]
 };
